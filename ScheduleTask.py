7d9c8ed8210388b869296edc2d9d693060078f38
wishyu <32516842+wishyu@users.noreply.github.com>
2021-12-20 00:28:26 +0800

    uploadftp

--- ScheduleTask.py
+++ ScheduleTask.py
@@ -1,7 +1,9 @@
 from CapstoneData import CovidData
+import FTPfunc
 
 a = CovidData()
 # a.UpdateLocalReportData()
 # a.GetSetCovidData(0)
 # a.ShowCovidData()
 a.PopulateTimeline()
+FTPfunc.upload_file()
