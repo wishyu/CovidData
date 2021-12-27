0503f82d19e43c9863e462c600d164281db96b26
wishyu <32516842+wishyu@users.noreply.github.com>
2021-12-27 14:49:04 +0800

    Schedclck

--- ScheduleTask.py
+++ ScheduleTask.py
@@ -1,11 +1,16 @@
 from CapstoneData import CovidData
 import FTPfunc
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-a = CovidData()
-# a.UpdateLocalReportData()
-# a.GetSetCovidData(0)
-# a.ShowCovidData()
-# a.PopulateTimeline()
-# FTPfunc.upload_file()
-FTPfunc.download_file()
-a.Weekly()
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', day_of_week='0-6', hour=20)
+def scheduled_job():
+    a = CovidData()
+    FTPfunc.download_file()
+    a.UpdateLocalReportData()
+    a.ShowCovidData()
+    FTPfunc.upload_file()
+    print('This job is run every weekday at 5pm.')
+
+sched.start()
