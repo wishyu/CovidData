ad2886d0eb86e0605fd40b3d24c902bb89d089a2
wishyu <32516842+wishyu@users.noreply.github.com>
2021-12-28 22:35:20 +0800

    schedtst

--- ScheduleTask.py
+++ ScheduleTask.py
@@ -4,10 +4,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='0-6', hour=22, minute=30)
+@sched.scheduled_job('cron', hour=22, minute=40)
 def scheduled_job():
-    a = CovidData()
     FTPfunc.download_file()
+    a = CovidData()
     a.UpdateLocalReportData()
     a.ShowCovidData()
     FTPfunc.upload_file()
