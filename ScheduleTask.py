ee825d055ad6f60e879dfbe17b3a0e4ad1968b64
wishyu <32516842+wishyu@users.noreply.github.com>
2021-12-30 21:26:28 +0800

    yes

--- ScheduleTask.py
+++ ScheduleTask.py
@@ -4,13 +4,20 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=16, minute=50)
-def scheduled_job():
-    print("5:00")
-    a = CovidData()
-    FTPfunc.download_file()
-    a.UpdateLocalReportData()
-    a.ShowCovidData()
-    FTPfunc.upload_file()
-    print('Tweet Me')
-sched.start()
+# @sched.scheduled_job('cron', hour=16, minute=50)
+# def scheduled_job():
+#     print("5:00")
+#     a = CovidData()
+#     FTPfunc.download_file()
+#     a.UpdateLocalReportData()
+#     a.ShowCovidData()
+#     FTPfunc.upload_file()
+#     print('Tweet Me')
+# sched.start()
+
+a = CovidData()
+FTPfunc.download_file()
+a.UpdateLocalReportData()
+a.ShowCovidData()
+FTPfunc.upload_file()
+print('Tweet Me')
