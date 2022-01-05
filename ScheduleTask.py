c08aef190be27afe6b016ace5825bcffe6246ea8
wishyu <32516842+wishyu@users.noreply.github.com>
2022-01-05 18:30:28 +0800

    Preparing for Weekly and Monthly Report

--- ScheduleTask.py
+++ ScheduleTask.py
@@ -6,11 +6,30 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', hour=16, minute=40)
 def scheduled_job():
-    print("4:00")
+    # Daily Report and Record Update @ 4:40pm
+    print("Updating Covid Data File and Posting to Twitter")
     a = CovidData()
     FTPfunc.download_file()
     a.UpdateLocalReportData()
-    a.ShowCovidData()
+    a.ShowCovidData() # Returns dict for twitter posting, add custom module
     FTPfunc.upload_file()
-    print('Tweet Me')
+    print("Tweet Me, api for tweeting")
+@sched.scheduled_job('cron', hour=16, minute=45, day_of_week="sat")
+def scheduled_job():
+    # Weekly Report @ 4:45 Saturday
+    print("Weekly Report and Posting to Twitter")
+    a = CovidData()
+    FTPfunc.download_file()
+    toPrint = a.Weekly() # Returns string for twitter posting, add custom module
+    FTPfunc.upload_file()
+    print(toPrint)
+@sched.scheduled_job('cron', day ="last sat", hour=16, minute=50, day_of_week="sat")
+def scheduled_job():
+    # Monthly Report @ 4:50 Saturday
+    print("Monthly Report and Posting to Twitter")
+    a = CovidData()
+    FTPfunc.download_file()
+    toPrint = a.Monthly() # Returns string for twitter posting, add custom module
+    FTPfunc.upload_file()
+    print(toPrint)
 sched.start()
