6b3b9ed8d1c13589b9e0ae8455fd641d56b12f0b
wishyu <32516842+wishyu@users.noreply.github.com>
2022-01-06 17:47:49 +0800

    Disable Week/Month insufficient Data

--- ScheduleTask.py
+++ ScheduleTask.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=17, minute=45)
+@sched.scheduled_job('cron', hour=17, minute=40)
 def scheduled_job():
     # Daily Report and Record Update @ 4:40pm
     print("Updating Covid Data File and Posting to Twitter")
@@ -14,20 +14,20 @@ def scheduled_job():
     a.ShowCovidData() # Returns dict for twitter posting, add custom module
     FTPfunc.upload_file()
     print("Tweet Me, api for tweeting")
-@sched.scheduled_job('cron', hour=16, minute=45, day_of_week="sat")
-def scheduled_job():
-    # Weekly Report @ 4:45 Saturday
-    print("Weekly Report and Posting to Twitter")
-    a = CovidData()
-    FTPfunc.download_file()
-    toPrint = a.Weekly() # Returns string for twitter posting, add custom module
-    print(toPrint)
-@sched.scheduled_job('cron', day ="last sat", hour=16, minute=50, day_of_week="sat")
-def scheduled_job():
-    # Monthly Report @ 4:50 Saturday
-    print("Monthly Report and Posting to Twitter")
-    a = CovidData()
-    FTPfunc.download_file()
-    toPrint = a.Monthly() # Returns string for twitter posting, add custom module
-    print(toPrint)
+# @sched.scheduled_job('cron', hour=17, minute=45, day_of_week="sat")
+# def scheduled_job():
+#     # Weekly Report @ 4:45 Saturday
+#     print("Weekly Report and Posting to Twitter")
+#     a = CovidData()
+#     FTPfunc.download_file()
+#     toPrint = a.Weekly() # Returns string for twitter posting, add custom module
+#     print(toPrint)
+# @sched.scheduled_job('cron', day ="last sat", hour=17, minute=50, day_of_week="sat")
+# def scheduled_job():
+#     # Monthly Report @ 4:50 Saturday
+#     print("Monthly Report and Posting to Twitter")
+#     a = CovidData()
+#     FTPfunc.download_file()
+#     toPrint = a.Monthly() # Returns string for twitter posting, add custom module
+#     print(toPrint)
 sched.start()
